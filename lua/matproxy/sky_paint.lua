289f6fdbce56087cd4ed4a5c3a6943e9fb36019e
friztailo <friztailo@gmail.com>
2024-10-31 00:42:28 +0500

    interesting

--- lua/matproxy/sky_paint.lua
+++ lua/matproxy/sky_paint.lua
@@ -6,6 +6,15 @@
     Significant reduction in garbage creation
 ]]--
 
+local meta = FindMetaTable( "Entity" )
+
+local GetDTVector = meta.GetDTVector
+local GetDTFloat = meta.GetDTFloat
+local GetDTBool = meta.GetDTBool
+local GetDTInt = meta.GetDTInt
+local GetDTAngle = meta.GetDTAngle
+local GetDTString = meta.GetDTString
+
 matproxy.Add( {
 	name = "SkyPaint",
 
@@ -17,30 +26,30 @@ matproxy.Add( {
 		local skyPaint = g_SkyPaint
 		if ( not IsValid( skyPaint ) ) then return end
 
-		mat:SetVector( "$TOPCOLOR", skyPaint:GetDTVector( 0 ) )
-		mat:SetVector( "$BOTTOMCOLOR", skyPaint:GetDTVector( 1 ) )
-		mat:SetVector( "$DUSKCOLOR", skyPaint:GetDTVector( 4 ) )
-		mat:SetFloat( "$DUSKSCALE", skyPaint:GetDTFloat( 2 ) )
-		mat:SetFloat( "$DUSKINTENSITY", skyPaint:GetDTFloat( 3 ) )
-		mat:SetFloat( "$FADEBIAS", skyPaint:GetDTFloat( 0 ) )
-		mat:SetFloat( "$HDRSCALE", skyPaint:GetDTFloat( 1 ) )
+		mat:SetVector( "$TOPCOLOR", GetDTVector( skyPaint, 0 ) )
+		mat:SetVector( "$BOTTOMCOLOR", GetDTVector( skyPaint, 1 ) )
+		mat:SetVector( "$DUSKCOLOR", GetDTVector( skyPaint, 4 ) )
+		mat:SetFloat( "$DUSKSCALE", GetDTFloat( skyPaint, 2 ) )
+		mat:SetFloat( "$DUSKINTENSITY", GetDTFloat( skyPaint, 3 ) )
+		mat:SetFloat( "$FADEBIAS", GetDTFloat( skyPaint, 0 ) )
+		mat:SetFloat( "$HDRSCALE", GetDTFloat( skyPaint, 1 ) )
 
-		mat:SetVector( "$SUNNORMAL", skyPaint:GetDTVector( 2 ) )
-		mat:SetVector( "$SUNCOLOR", skyPaint:GetDTVector( 3 ) )
-		mat:SetFloat( "$SUNSIZE", skyPaint:GetDTFloat( 4 ) )
+		mat:SetVector( "$SUNNORMAL", GetDTVector( skyPaint, 2 ) )
+		mat:SetVector( "$SUNCOLOR", GetDTVector( skyPaint, 3 ) )
+		mat:SetFloat( "$SUNSIZE", GetDTFloat( skyPaint, 4 ) )
 
-		if ( not skyPaint:GetDTBool( 0 ) ) then
+		if ( not GetDTBool( skyPaint, 0 ) ) then
 			return mat:SetInt( "$STARLAYERS", 0 )
 		end
 
-		mat:SetInt( "$STARLAYERS", skyPaint:GetDTInt( 0 ) )
+		mat:SetInt( "$STARLAYERS", GetDTInt( skyPaint, 0 ) )
 
-		local star = skyPaint:GetDTAngle( 0 )
+		local star = GetDTAngle( skyPaint, 0 )
 		mat:SetFloat( "$STARSCALE", star.p )
 		mat:SetFloat( "$STARFADE", star.y )
 		mat:SetFloat( "$STARPOS", star.r * RealTime() )
 
-		mat:SetTexture( "$STARTEXTURE",	skyPaint:GetDTString( 0 ) )
+		mat:SetTexture( "$STARTEXTURE",	GetDTString( skyPaint, 0 ) )
 
 	end
 } )
