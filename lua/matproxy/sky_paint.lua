e40cc41aa96cb90ed6299c9798e4fdf8004eed18
friztailo <friztailo@gmail.com>
2024-11-02 02:31:41 +0500

    oops

--- lua/matproxy/sky_paint.lua
+++ lua/matproxy/sky_paint.lua
@@ -63,32 +63,39 @@ matproxy.Add( {
 		local skyPaint = g_SkyPaint
 		if not IsValid( skyPaint ) then return end
 
-		mat:SetVector( "$TOPCOLOR", skyPaint:GetDTVector( 0 ) )
-		mat:SetVector( "$BOTTOMCOLOR", skyPaint:GetDTVector( 1 ) )
-		mat:SetVector( "$DUSKCOLOR", skyPaint:GetDTVector( 4 ) )
-		mat:SetFloat( "$DUSKSCALE", skyPaint:GetDTFloat( 2 ) )
-		mat:SetFloat( "$DUSKINTENSITY", skyPaint:GetDTFloat( 3 ) )
-		mat:SetFloat( "$FADEBIAS", skyPaint:GetDTFloat( 0 ) )
-		mat:SetFloat( "$HDRSCALE", skyPaint:GetDTFloat( 1 ) )
+		local SetVector = mat.SetVector
+		local SetFloat = mat.SetFloat
+		local SetInt = mat.SetInt
+		local SetTexture = mat.SetTexture
 
-		mat:SetVector( "$SUNNORMAL", skyPaint:GetDTVector( 2 ) )
-		mat:SetVector( "$SUNCOLOR", skyPaint:GetDTVector( 3 ) )
-		mat:SetFloat( "$SUNSIZE", skyPaint:GetDTFloat( 4 ) )
+		self.bind = function( self, mat, ent )
 
-		if ( skyPaint:GetDTBool( 0 ) ) then
-			mat:SetInt( "$STARLAYERS", skyPaint:GetDTInt( 0 ) )
+			SetVector( mat, "$TOPCOLOR", skyPaint:GetDTVector( 0 ) )
+			SetVector( mat, "$BOTTOMCOLOR", skyPaint:GetDTVector( 1 ) )
+			SetVector( mat, "$DUSKCOLOR", skyPaint:GetDTVector( 4 ) )
+			SetFloat( mat, "$DUSKSCALE", skyPaint:GetDTFloat( 2 ) )
+			SetFloat( mat, "$DUSKINTENSITY", skyPaint:GetDTFloat( 3 ) )
+			SetFloat( mat, "$FADEBIAS", skyPaint:GetDTFloat( 0 ) )
+			SetFloat( mat, "$HDRSCALE", skyPaint:GetDTFloat( 1 ) )
 
-			local star = skyPaint:GetDTAngle( 0 )
-			mat:SetFloat( "$STARSCALE", star[1] )
-			mat:SetFloat( "$STARFADE", star[2] )
-			mat:SetFloat( "$STARPOS", star[3] * RealTime() )
+			SetVector( mat, "$SUNNORMAL", skyPaint:GetDTVector( 2 ) )
+			SetVector( mat, "$SUNCOLOR", skyPaint:GetDTVector( 3 ) )
+			SetFloat( mat, "$SUNSIZE", skyPaint:GetDTFloat( 4 ) )
 
-			mat:SetTexture( "$STARTEXTURE", skyPaint:GetDTString( 0 ) )
-		else
-			mat:SetInt( "$STARLAYERS", 0 )
+			if ( skyPaint:GetDTBool( 0 ) ) then
+				SetInt( mat, "$STARLAYERS", skyPaint:GetDTInt( 0 ) )
+
+				local star = skyPaint:GetDTAngle( 0 )
+				SetFloat( mat, "$STARSCALE", star[1] )
+				SetFloat( mat, "$STARFADE", star[2] )
+				SetFloat( mat, "$STARPOS", star[3] * RealTime() )
+	
+				SetTexture( mat, "$STARTEXTURE", skyPaint:GetDTString( 0 ) )
+			else
+				SetInt( mat, "$STARLAYERS", 0 )
+			end
+
 		end
-
-		self.bind = nil
 
 	end
 } )
